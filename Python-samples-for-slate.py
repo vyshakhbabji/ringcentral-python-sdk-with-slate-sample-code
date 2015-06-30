6ea6d94f6c2070a1a2af83d6bdc7bd6a42c91c75
vyshakhbabji <vyshakh.babji@gmail.com>
2015-06-30 15:26:25 -0700

    Commit all

--- Python-samples-for-slate.py
+++ Python-samples-for-slate.py
@@ -9,11 +9,11 @@ from rcsdk.subscription import EVENTS
 #Instantiating the SDK
 RC_SERVER_PRODUCTION = 'https://platform.ringcentral.com'
 RC_SERVER_SANDBOX = 'https://platform.devtest.ringcentral.com'
-YOUR_APPKEY = 'E0_nOAfbR7GkteYbDv93oA'
-YOUR_APPSECRET = 'UelNnk-1QYK0rHyvjJJ9yQx3Yl6vj3RvGmb0G2SH6ePw'
-YOUR_PHONENUMBER= '15856234138'
+YOUR_APPKEY = ''
+YOUR_APPSECRET = ''
+YOUR_PHONENUMBER= ''
 YOUR_EXTENSION=''
-YOUR_PASSWORD= 'P@ssw0rd'
+YOUR_PASSWORD= ''
 sdk = RCSDK(YOUR_APPKEY, YOUR_APPSECRET , RC_SERVER_SANDBOX)
 
 
@@ -61,30 +61,30 @@ sms_body = {"to":[{"phoneNumber":to_Number}],"from":{"phoneNumber":from_Number},
 response = platform.post(url='/account/~/extension/~/sms',body=sms_body)
 print('SMS Information: ' + str(response.get_body()))
 
-#RingOut: Making Calls
-# to_Number = '16197619503'
-# from_Number =  '16505154891'
-# caller_Id = '15856234138'
-# ringout_body = {
-#   "to": {"phoneNumber": to_Number},
-#   "from": {"phoneNumber": from_Number},
-#   "callerId": {"phoneNumber": caller_Id},
-#   "playPrompt": "true"
-# }
-#
-#
-# response = platform.post(url='/account/~/extension/~/ringout',body=ringout_body)
-# print('RingOut Information: ' + str(response.get_body()))
-# ringout = response.get_json(True)
+# RingOut: Making Calls
+to_Number = '16197619503'
+from_Number =  '16505154891'
+caller_Id = '15856234138'
+ringout_body = {
+  "to": {"phoneNumber": to_Number},
+  "from": {"phoneNumber": from_Number},
+  "callerId": {"phoneNumber": caller_Id},
+  "playPrompt": "true"
+}
 
 
-#RingOut: Disconnect Calls
-# ringout_Id = str(ringout.id);
-# print ringout_Id
-# time.sleep(5)
-#
-# response = platform.delete(url = '/account/~/extension/~/ringout/'+ringout_Id)
-# print('RingOut Delete Information : ' + str(response.get_status()))
+response = platform.post(url='/account/~/extension/~/ringout',body=ringout_body)
+print('RingOut Information: ' + str(response.get_body()))
+ringout = response.get_json(True)
+
+
+# RingOut: Disconnect Calls
+ringout_Id = str(ringout.id);
+print ringout_Id
+time.sleep(5)
+
+response = platform.delete(url = '/account/~/extension/~/ringout/'+ringout_Id)
+print('RingOut Delete Information : ' + str(response.get_status()))
 
 
 #Presence
